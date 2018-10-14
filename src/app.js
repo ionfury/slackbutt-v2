3c3d570b66d7e8db31bf23da4163ef27cd24f32a
Thomas Nowak <tnowak@plex.com>
2018-10-14 18:54:08 -0400

    fixed my dumb

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ const Discord = require(`discord.js`);
 const Client = new Discord.Client();
 const Promise = require('bluebird');
 const MarkovService = require('./services/markov-service');
-
 Client.on('ready', () => {
-
+console.log("connected")
 });
 
 Client.on('message', msg => {
   if(msg.author.bot) return;
 
   MarkovService.Consider(msg.content)
-    .then(res => res != null ? msg.channel.send(res) : null)
-    .catch(err => msg.channel.send(dump(err)));
+    //.then(console.log)
+   .then(res => res != null ? msg.channel.send(res.string) : null)
+   .catch(err => msg.channel.send(dump(err)));
 });
 
 Client.login(process.env.token);
--- src/services/markov-service.js
+++ src/services/markov-service.js
@@ -14,31 +14,38 @@ module.exports = {
   }
 }
 function ExtractStrings(arr) {
+  console.log(`extract`);
   if(!arr.map) return;
   return arr.map(a => a.string);
 }
 
 function BuildMarkov(strings) {
+  console.log(`build markov`);
   if(!strings) return null;
+  console.log(strings);
+
   let markov = new Markov(strings, Config.markovDefaultOptions);
   markov.buildCorpus();
   return markov;
 }
 
 function ShouldRespond(text) {
+  console.log(`should respond`);
   return text.toLowerCase().includes(Config.botname) || 
     Math.floor((Math.random() * 100) < parseInt(Config.responsechance));
 }
 
 function ShouldSmartReply() {
+  console.log(``);
   return Math.floor((Math.random() * 100) < parseInt(Config.longresponsechance))
 }
 
 function Respond(markov, input) {
+  console.log(`respond`);
   if(!markov) return null;
   try
   {
-    console.log('smart reply');
+    console.log('smart');
     return SmartReply(markov, input);
   }
   catch (err)
@@ -49,6 +56,7 @@ function Respond(markov, input) {
 }
 
 function SmartReply(markov, input) {
+  console.log(`smart reply`);
   let longestWord = input.split(' ').sort((a, b) => a.length - b.length);
   let smartOptions = {
     filter: (res) => res.string.includes(longestWord)
@@ -57,5 +65,6 @@ function SmartReply(markov, input) {
 }
 
 function Reply(markov) {
+  console.log(`reply`);
   return markov.generateSentenceSync();
 }
